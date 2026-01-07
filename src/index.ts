eb0a2e61aae6f4c071054e694112ba1867034cf3
MIND <79649658+manu-neg@users.noreply.github.com>
2026-01-07 15:26:45 -0400

    Auth validator

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { connectDB } from './config/database';
 import { AuthController } from './controllers/AuthController';
 import { ImageController } from './services/ImageService';
+import AuthEndpointValidator from './controllers/AuthEndpointValidator';
 
 // Configuración
 dotenv.config();
@@ -14,15 +15,22 @@ app.use(cors());
 // Conectar Base de Datos
 connectDB();
 
+// TODO: reemplazar esta funcion con la validacion de tokens real
+const validationFn = (req: express.Request): boolean => {
+  let va: boolean = true;
+    console.log("validation: ", va);
+    return va;
+};
+
 // Rutas (Definirlas aquí directo para ir rápido por ahora)
 const router = express.Router();
 router.post('/auth/register', AuthController.register);
 router.post('/auth/login', AuthController.login);
-router.post('/images/resize', ImageController.processResize);
-router.post('/images/crop', ImageController.processCrop);
-router.post('/images/rotate', ImageController.processRotate);
-router.post('/images/filter', ImageController.processFilter);
-router.post('/images/pipeline', ImageController.processRequestPipeline);
+router.post('/images/resize', AuthEndpointValidator.bind(ImageController.processResize, validationFn));
+router.post('/images/crop', AuthEndpointValidator.bind(ImageController.processCrop, validationFn));
+router.post('/images/rotate', AuthEndpointValidator.bind(ImageController.processRotate, validationFn));
+router.post('/images/filter', AuthEndpointValidator.bind(ImageController.processFilter, validationFn));
+router.post('/images/pipeline', AuthEndpointValidator.bind(ImageController.processRequestPipeline, validationFn));
 app.use('/api', router);
 
 const PORT = process.env.PORT || 3000;
